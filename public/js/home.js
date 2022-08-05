8e19886cdbee4b909e6a6f55ff497064f883792d
Jose V <valdezjose856@gmail.com>
2022-08-05 05:19:24 -0500

    test update

--- public/js/home.js
+++ public/js/home.js
@@ -13,7 +13,7 @@ const createBlog = (blog) => {
     blogSection.innerHTML += ` 
         <div class="blog-card">
             <img src="${data.bannerImage}" class="blog-image" alt="">
-            <h1 class="blog-title">${data.title.substring(0,100) + '...'}</h1>
+            <h1 class="blog-title">${data.title.substring(0, 100) + '...'}</h1>
             <p class="blog-overview">${data.article.substring(0, 200) + '...'}</p>
             <a href="/${blog.id}" class="btn dark">read more</a>
         </div>
